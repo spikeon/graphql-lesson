9b2baf1d9af8d35a1ce2c9c5a3e940a323668b04
Mike Flynn <spikeon@gmail.com>
2021-02-04 11:48:26 -0800

    Implementing Apollo on Shop page and Collection page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 import App from './App';
 import {createHttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 
 const httpLink = createHttpLink({
     uri: 'https://crwn-clothing.com'
